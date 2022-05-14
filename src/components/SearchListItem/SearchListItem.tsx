de8bfde2e963e138fbf853eeda86e54829392bfd
David Vass <nighturbex@protonmail.com>
2022-05-14 21:44:24 +0200

    feat: handle img error, resposive design, csp

--- src/components/SearchListItem/SearchListItem.tsx
+++ src/components/SearchListItem/SearchListItem.tsx
@@ -7,16 +7,22 @@ interface SearchListItemInterface {
     ingredients: string[]
 }
 
-export const SearchListItem: React.FC<SearchListItemInterface> = ({ name, image, ingredients }) => (
-  <li className="search-list__item">
-    <figure className="search-list__item-figure">
-      <img src={image} alt={name} />
-      <figcaption>{name}</figcaption>
-    </figure>
-    <ul className="search-list__item-ingredients">
-      {ingredients.map((ingredient) => (
-        <li key={`${name}_${ingredient}`}>{ingredient}</li>
-      ))}
-    </ul>
-  </li>
-);
+export const SearchListItem: React.FC<SearchListItemInterface> = ({ name, image, ingredients }) => {
+  const handleImageError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = `${process.env.PUBLIC_URL}/not-found.png`;
+  };
+
+  return (
+    <li className="search-list__item">
+      <figure className="search-list__item-figure">
+        <img src={image} alt={name} onError={handleImageError} />
+        <figcaption>{name}</figcaption>
+      </figure>
+      <ul className="search-list__item-ingredients">
+        {ingredients.map((ingredient) => (
+          <li key={`${name}_${ingredient}`}>{ingredient}</li>
+        ))}
+      </ul>
+    </li>
+  );
+};
